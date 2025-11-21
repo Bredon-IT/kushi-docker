717ef051451ab9954ccd6dbb9724e480beeaa777
Bredon-IT <bredon.github@gmail.com>
2025-11-21 18:43:16 +0530

    Delete src/main/java/com/kushi/in/app/service/CategoryWiseBookingService.java

--- src/main/java/com/kushi/in/app/service/CategoryWiseBookingService.java
+++
@@ -1,8 +0,0 @@
-package com.kushi.in.app.service;
-
-import com.kushi.in.app.model.CategoryWiseBookingDTO;
-import java.util.List;
-
-public interface CategoryWiseBookingService {
-    List<CategoryWiseBookingDTO> getCategoryWiseBookings();
-}
