1650b2b9c761e09a514a22e1b9973b034f53cad8
Narayanaswamy <mrswamy1987@gmail.com>
2025-12-12 17:14:34 +0530

    Global url

--- src/main/java/com/kushi/in/app/constants/KushiConstants.java
+++ src/main/java/com/kushi/in/app/constants/KushiConstants.java
@@ -5,6 +5,6 @@ public interface KushiConstants {
     String KUSHI_WEB_URL = "https://www.kushiservices.com";
     String KUSHI_URL = "https://kushiservices.com";
     String COMMA = ",";
-    String KUSHI_GLOBAL = KUSHI_LOCAL;
-    // String KUSHI_GLOBAL = KUSHI_WEB_URL + COMMA + KUSHI_URL;
+    //String KUSHI_GLOBAL = KUSHI_LOCAL;
+     String KUSHI_GLOBAL = KUSHI_WEB_URL + COMMA + KUSHI_URL;
 }
